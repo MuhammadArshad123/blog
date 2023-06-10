4f481cb81af694f4b5939e6410c1653284b9d0b8
Muhammad Arshad <63872178+MuhammadArshad123@users.noreply.github.com>
2023-06-10 01:18:41 -0500

    First commit

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,40 @@
+<header>
+  <a href="/">Home</a>
+
+  <nav>
+    <ul>
+      <li>
+        <a href="/blog">Blog</a>
+      </li>
+
+      
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
+
+<style>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: skyblue;
+    padding: 0.5rem;
+  }
+
+  ul {
+    display: flex;
+    list-style: none;
+    gap: 1rem;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Header from "$lib/components/Header.svelte";
+  import Footer from "$lib/components/Footer.svelte";
+  import "$lib/styles/styles.scss";
+</script>
+
+<Header />
+<main>
+  <slot />
+</main>
+<Footer />
