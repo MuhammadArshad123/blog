9bf6431c790fca1678aec0ff233a491cf81712ac
Muhammad Arshad <63872178+MuhammadArshad123@users.noreply.github.com>
2023-06-13 12:24:28 -0500

    added log-in fomr

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,9 @@
-<header class="p-4 flex flex-wrap justify-between bg-slate-100 text-xl mb-8">
+<script>
+</script>
+
+<header
+  class="p-4 flex flex-wrap justify-between text-xl mb-8 bg-blue-400 text-darkgray"
+>
   <a href="/" class="font-extrabold">Home</a>
   <nav>
     <ul class="list-none flex m-0 gap-5 font-bold">
@@ -10,6 +15,13 @@
       </li>
       <li>
         <a href="/contact">Contact</a>
+      </li>
+      <li>
+        <a href="/login">
+          <button class="pl-2 pr-2 bg-blue-800 rounded text-white"
+            >Log In</button
+          >
+        </a>
       </li>
     </ul>
   </nav>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,45 @@
+<script>
+  function onSubmit(e) {
+    const formData = new FormData(e.target);
+    const data = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+    console.log(data);
+  }
+</script>
+
+<main class="flex justify-center">
+  <section class="max-w-3xl mx-auto">
+    <div class="flex justify-center">
+      <h1 class="text-3xl font-bold mb-4">Log In</h1>
+    </div>
+    <form on:submit|preventDefault={onSubmit} action="">
+      <div class="mb-4">
+        <input
+          type="text"
+          name="email"
+          id="email"
+          class="outline"
+          placeholder="Email"
+        />
+      </div>
+      <div class="mb-6">
+        <input
+          type="text"
+          name="password"
+          id="password"
+          class="outline"
+          placeholder="Password"
+        />
+      </div>
+      <div class="flex justify-center">
+        <button
+          type="submit"
+          class="bg-blue-800 text-white outline-1 rounded p-3">Submit</button
+        >
+      </div>
+    </form>
+  </section>
+</main>
